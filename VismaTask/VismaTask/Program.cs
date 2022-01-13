fc19f97f0c5e4e9f00875f7f42bdad2bf47b2b95
Germis2021 <89936809+Germis2021@users.noreply.github.com>
2022-01-13 21:34:01 +0200

    Navigacija ir failo skaituymas

--- VismaTask/VismaTask/Program.cs
+++ VismaTask/VismaTask/Program.cs
@@ -6,7 +6,47 @@ namespace VismaTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            bool exite = false;
+            int selection = 0;
+            DB.Load();
+           
+            while (!exite)
+            {
+                Console.WriteLine("Pasirinkimai");
+                Console.WriteLine("1-Prideti nauja susitikima");
+                Console.WriteLine("2-Istrinti susitikima");
+                Console.WriteLine("3- Prideti zmogu prie susitikimo");
+                Console.WriteLine("4-Istrinti zmogu is susitikimo");
+                Console.WriteLine("5-Perziureti visus susitikimus");
+                Console.WriteLine("6- Baigti programa");
+
+                if (!int.TryParse(Console.ReadLine(), out selection) || selection >6 || selection < 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Netinkamai ivestas pasirinkimas.");
+                    continue;
+
+                }
+
+                switch (selection)
+                {
+                    case 1: MeetingController.Create();
+                        break;
+                    case 2: MeetingController.Delete();
+                        break;
+                    case 3: MeetingController.AddPerson();
+                        break;
+                    case 4: MeetingController.RemovePerson();
+                        break;
+                    case 5: MeetingController.GetAll();
+                        break;
+                    default: Console.Clear();
+                        exite = true;
+                        break;
+                }
+
+            }
         }
     }
 }
---
+++ VismaTask/VismaTask/UI_Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VismaTask
+{
+    public static class UI_Helper
+    {
+
+
+    }
+}
